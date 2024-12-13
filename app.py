865af7f0010964abfaf6d1d8700833194e15c343
mohitkumar1313 <kumar.mohit3381@gmail.com>
2024-12-13 18:13:16 -0500

    changing code and adding dockerfile

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, redirect
 from api import api_bp
-from database import insert_static_data 
+from database import insert_static_data
 from flasgger import Swagger
 
 app = Flask(__name__)
@@ -10,6 +10,11 @@ insert_static_data()
 swagger = Swagger(app, template_file='swagger.yml')
 
 app.register_blueprint(api_bp)
+
+# Redirect root URL to Swagger UI
+@app.route('/')
+def redirect_to_docs():
+    return redirect('/apidocs/')
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=5000)
